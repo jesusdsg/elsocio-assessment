16d7441d6536b516674e278f480df9c6c0ddcf3a
jesusdsg <jesalceg@gmail.com>
2024-06-20 23:35:58 -0500

    feat: find by query feature

--- app/components/MealsCategories.tsx
+++ app/components/MealsCategories.tsx
@@ -7,6 +7,7 @@ export default function MealsCategories() {
   const [categories, setCategories] = useState<Category[]>([]);
   const selectCategory = useStore((state) => state.selectCategory);
   const selectedCategory = useStore((state) => state.selectedCategory);
+  const setMealQuery = useStore((state) => state.setMealQuery);
 
   /**
    * Insert the all category object
@@ -30,6 +31,10 @@ export default function MealsCategories() {
   };
   setAllCategory();
 
+  const handleQueryChange = (event: any) => {
+    if (event.key == "Enter") setMealQuery(event.target.value);
+  };
+
   /**
    * Get the categories Data
    */
@@ -50,31 +55,41 @@ export default function MealsCategories() {
   }, [selectedCategory]);
 
   return (
-    <div className="grid grid-cols-2 md:flex lg:flex gap-2 w-full flex-wrap">
-      {categories.map((category) => (
-        <button
-          className={`px-6 py-1 rounded-md text-xs lg:text-sm flex gap-1 hover:text-gray-700 duration-300 items-center ${
-            selectedCategory?.idCategory == category.idCategory
-              ? "bg-orange-500 text-white"
-              : "bg-gray-100 text-gray-500"
-          } `}
-          key={category.idCategory}
-          onClick={() => selectCategory(category)}
-        >
-          {category.strCategoryThumb != "" && (
-            <div className="relative h-6 w-6">
-              <Image
-                src={category.strCategoryThumb}
-                alt={category.strCategory}
-                layout="fill"
-                objectFit="cover"
-                className="rounded-md"
-              />
-            </div>
-          )}
-          {category.strCategory}
-        </button>
-      ))}
-    </div>
+    <>
+      {/* Search for name, use enter to make de query search */}
+      <input
+        type="text"
+        placeholder="Search..."
+        className="bg-gray-100 px-2 py-1 rounded absolute top-[10vh] lg:top-[20vh] right-10 lg:right-[6vw] border"
+        onKeyUp={(e) => handleQueryChange(e)}
+      />
+      {/* Categories */}
+      <div className="grid grid-cols-2 md:flex lg:flex gap-2 w-full flex-wrap">
+        {categories.map((category) => (
+          <button
+            className={`px-6 py-1 rounded-md text-xs lg:text-sm flex gap-1 hover:text-gray-700 duration-300 items-center ${
+              selectedCategory?.idCategory == category.idCategory
+                ? "bg-orange-500 text-white"
+                : "bg-gray-100 text-gray-500"
+            } `}
+            key={category.idCategory}
+            onClick={() => selectCategory(category)}
+          >
+            {category.strCategoryThumb != "" && (
+              <div className="relative h-6 w-6">
+                <Image
+                  src={category.strCategoryThumb}
+                  alt={category.strCategory}
+                  layout="fill"
+                  objectFit="cover"
+                  className="rounded-md"
+                />
+              </div>
+            )}
+            {category.strCategory}
+          </button>
+        ))}
+      </div>
+    </>
   );
 }
--- app/store.ts
+++ app/store.ts
@@ -1,21 +1,23 @@
 // src/store.ts (o store.js si no usas TypeScript)
-import { create } from 'zustand';
-import { Category } from './utils/types/category';
-
-
+import { create } from "zustand";
+import { Category } from "./utils/types/category";
 
 interface State {
-    categories: Category[];
-    selectedCategory: Category | null;
-    setCategories: (categories: Category[]) => void;
-    selectCategory: (category: Category) => void;
+  categories: Category[];
+  selectedCategory: Category | null;
+  mealQuery: string;
+  setCategories: (categories: Category[]) => void;
+  selectCategory: (category: Category) => void;
+  setMealQuery: (query: string) => void;
 }
 
 const useStore = create<State>((set) => ({
-    categories: [],
-    selectedCategory: null,
-    setCategories: (categories) => set({ categories }),
-    selectCategory: (category) => set({ selectedCategory: category }),
+  categories: [],
+  selectedCategory: null,
+  mealQuery: "",
+  setCategories: (categories) => set({ categories }),
+  selectCategory: (category) => set({ selectedCategory: category }),
+  setMealQuery: (query: string) => set({ mealQuery: query }),
 }));
 
 export default useStore;
